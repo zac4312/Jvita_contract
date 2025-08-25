b0125dafa8d89bff4ffd7d11b21505baef1d0216
zac4312 <studentu617@gmail.com>
2025-08-25 11:49:08 +0800

    renamed uto to filing | added delete image when update and delete imgfile on dlt functions | added IOexception Handling

--- src/main/java/com/jvita/truck/service/CMSService.java
+++ src/main/java/com/jvita/truck/service/CMSService.java
@@ -3,7 +3,7 @@ package com.jvita.truck.service;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.jvita.truck.util.Util; 
+import com.jvita.truck.util.Filing; 
 import com.jvita.truck.model.CMSModel;
 import com.jvita.truck.repository.CMSRepository;
 
@@ -15,21 +15,18 @@ import java.util.Optional;
 public class CMSService {
 
     private final CMSRepository cmsRepository;
-    private final Util util;
+    private final Filing FilingTool;
 
-    public CMSService(CMSRepository cmsRepository, Util util){ this.cmsRepository = cmsRepository; this.util = util;}
+    public CMSService(CMSRepository cmsRepository, Filing FilingTool){ this.cmsRepository = cmsRepository; this.FilingTool = FilingTool;}
 
 
     public CMSModel createModel(CMSModel model, MultipartFile imgFile) throws IOException {
 
-        if (imgFile != null && !imgFile.isEmpty()) {
-            model.setImgFile(util.uploadImg(imgFile));
+        if (!imgFile.isEmpty() || imgFile != null) {
+            model.setImgFile(FilingTool.uploadImg(imgFile));    
         }
+        
 
-        System.out.println(" ");
-        System.out.println("SERVICE");
-        System.out.println("MODEL: " + model);
-        System.out.println("IMAGE: " + model.getImgFile());
         return cmsRepository.save(model);   
     }
 
@@ -37,28 +34,34 @@ public class CMSService {
         return cmsRepository.findAll();
     }
 
-    public Optional<CMSModel> IdModel(Long id) {
-        return cmsRepository.findById(id);
-    }
+    public CMSModel getModel(Long id) {
+        return cmsRepository.findById(id).orElseThrow(() -> 
+            new IllegalArgumentException("Model: " + id  + "NOT FOUND"));
+    }                   
 
-    public void dltModel(Long id){
+    public void dltModel(Long id) throws IOException {
+        CMSModel model = getModel(id);
+        FilingTool.DeleteImg(model.getImgFile());
         cmsRepository.deleteById(id) ;
     }
 
-    public void dltALL(){
-        cmsRepository.deleteAll(); ;
+    public void dltALL() throws IOException{
+        for (CMSModel model : cmsRepository.findAll()) {        
+            FilingTool.DeleteImg(model.getImgFile());
+        }
+
+        cmsRepository.deleteAll();
     }
 
     public CMSModel editModel(CMSModel newData, MultipartFile imgFile, Long id) throws IOException {
-    CMSModel model = cmsRepository.findById(id).orElseThrow(() -> 
-        new IllegalArgumentException("Item does not exist")
-    );
+    CMSModel model = getModel(id);
 
     model.setName(newData.getName());
     model.setDescription(newData.getDescription());
 
     if (imgFile != null && !imgFile.isEmpty()) {
-            model.setImgFile(util.uploadImg(imgFile));
+            FilingTool.DeleteImg(model.getImgFile());
+            model.setImgFile(FilingTool.uploadImg(imgFile));
         }
 
     return cmsRepository.save(model);
